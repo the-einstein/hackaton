afc90688c38ff7e4e88b5832b4d5e5ec817e3ec7
Shamsiddin <quyosh.ogli@gmail.com>
2020-12-04 19:38:44 +0500

    fix: fix routes

--- router/home.js
+++ router/home.js
@@ -2,11 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/next", (req, res) => {
-  res.send("/ route");
+router.get("/", (req, res) => {
+  res.send("/ home");
 });
 
-router.get("/", (req, res) => {
+router.get("/next", (req, res) => {
   res.send("/ next");
 });
 
