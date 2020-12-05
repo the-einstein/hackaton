426dcc44fb3a077eaa3a2b88f22bbcd5ce4144cb
Shamsiddin <quyosh.ogli@gmail.com>
2020-12-05 16:06:13 +0500

    feat: add user router

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ const app = express();
 
 //routers
 const homeRoute = require("./router/home");
+const userRouter = require("./router/user");
 
 app.use("/", homeRoute);
+
+app.use("/user", userRouter);
 
 app.listen(process.env.PORT || 8000, (err) => {
   if (err) console.log(`there is an error \n ${err}`);
