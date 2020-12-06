9b16c318c4a6753518ca9aeec93a048df1a4f7f2
Shamsiddin <quyosh.ogli@gmail.com>
2020-12-06 16:44:55 +0500

    fix: add scores for each organ

--- router/organ.js
+++ router/organ.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const Organ = require("../model/Organ");
 const User = require("../model/User");
+const App = require("../model/Application");
 const Region = require("../model/Region");
 const auth = require("../verifyToken");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const organs = await Organ.aggregate([
-    {
-      $lookup: {
-        from: "application",
-        localField: "_id",
-        foreignField: "to",
-        as: score,
-      },
-    },
-  ]);
-  console.log(organs);
+  //TODO: refactor code to make it work with aggregate function to optimize it
+  // const organs = await Organ.aggregate([
+  //   {
+  //     $lookup: {
+  //       from: "application",
+  //       localField: "_id",
+  //       foreignField: "to",
+  //       as: "score",
+  //     },
+  //   },
+  // ]);
+  const organs = await Organ.find();
+  for (organ in organs) {
+    let scores = await App.find({ to: organs[organ]._id });
+    let sumScore = 0;
+    let midScore = 0;
+    for (score in scores) {
+      sumScore = sumScore + scores[score].score;
+    }
+    midScore = sumScore / scores.length;
+    organs[organ].score = midScore;
+  }
   res.send(organs);
 });
 
